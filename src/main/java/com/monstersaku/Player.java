581a64940a2afc821f2631c9b133af55b695fe48
fikfikriii <18220097@std.stei.itb.ac.id>
2022-04-09 13:59:41 +0700

    Update Player.java

--- src/main/java/com/monstersaku/Player.java
+++ src/main/java/com/monstersaku/Player.java
@@ -28,6 +28,10 @@ public class Player {
         return this.monsters;
     }
 
+    public MonsterPool getSwitchingOption(){
+        return this.switchingOption;
+    }
+
     public Monster getCurrentMonster(){
         return this.currentMonster;
     }
@@ -54,17 +58,26 @@ public class Player {
             System.out.println("Getting random monster for " + this.name + " ....");
             //Thread.sleep(3000);
             List<Monster> copyMonsterPool = listMonster.getListMonster();
+            List<Monster> copyMonsterPool2 = listMonster.getListMonster();
             List<Monster> copyList = new ArrayList<Monster>(copyMonsterPool);
+            List<Monster> copyList2 = new ArrayList<Monster>(copyMonsterPool2);
             List<Monster> randomized = new ArrayList<Monster>();
+            List<Monster> randomized2 = new ArrayList<Monster>();
             for (int j = 0; j < i; j++){
                 int index = new Random().nextInt(copyList.size());
                 Monster monster = copyList.get(index);
+                Monster monster2 = copyList2.get(index);
                 randomized.add(monster);
+                randomized2.add(monster2);
                 copyList.remove(index);
+                copyList2.remove(index);
             }
             MonsterPool mp = new MonsterPool();
+            MonsterPool mp2 = new MonsterPool();
             mp.setListMonster(randomized);
+            mp2.setListMonster(randomized2);
             this.monsters = mp;
+            this.switchingOption = mp2;
         } catch (Exception e) {
             System.out.println("Main interrupted");
         }
@@ -78,19 +91,18 @@ public class Player {
             Thread.sleep(10);
             int index = new Random().nextInt(monsters.getSize());
             currentMonster = (monsters.getListMonster()).get(index);
+            this.switchOption();
         } catch(InterruptedException e) {
             System.out.println("Main interrupted");
         }
-        
     }
 
     public void switchOption(){
         MonsterPool mp = monsters;
-        List<Monster> lm = monsters.getListMonster();
+        List<Monster> lm = switchingOption.getListMonster();
         lm.remove(this.getCurrentMonster());
         mp.setListMonster(lm);
         this.switchingOption = mp;
-        mp.printMonster();
     }
 
     public void getSwitchMonster(int x){
@@ -98,11 +110,13 @@ public class Player {
         for (int i = 1; i <= switchingOption.getSize(); i++){
             if (i==x){
                 m = (switchingOption.getListMonster()).get(i-1);
+                break;
             }
         }
         this.switchingOption.addMonster(this.getCurrentMonster());
         System.out.printf("Successfully changed monster to %s%n", m.getMonsterName());
         this.currentMonster = m;
+        this.switchingOption.getListMonster().remove(m);
         System.out.println();
     }
 
@@ -115,6 +129,7 @@ public class Player {
         }
         System.out.printf("%s sent out %s!%n", this.name, m.getMonsterName());
         this.currentMonster = m;
+        this.switchingOption.getListMonster().remove(m);
         System.out.println();
     }
 

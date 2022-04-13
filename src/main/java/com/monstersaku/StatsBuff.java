577d2bae0565a0d8adf0d116a0c0a96aa005a42f
fikfikriii <18220097@std.stei.itb.ac.id>
2022-04-13 13:05:50 +0700

    bonus (stats buff) interface

---
+++ src/main/java/com/monstersaku/StatsBuff.java
@@ -0,0 +1,9 @@
+package com.monstersaku;
+
+/* Bonus 
+   Diimplementasikan pada class Move
+   Pemanggilan dilakukan pada class Monster */
+
+public interface StatsBuff {
+    public double getFactor(int x);
+}
